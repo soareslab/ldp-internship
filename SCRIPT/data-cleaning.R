5cf1fbded9863fe9ea0f1743fe415e4aa8edc9b1
Rodrigo Martin de Oliveira <hodorigo@hotmail.com>
2025-12-01 15:16:03 -0600

    renaming binders

--- SCRIPT/data-cleaning.R
+++ SCRIPT/data-cleaning.R
@@ -9,36 +9,43 @@ renv::snapshot()
 
 
 # Load resighting data from CSV files into R data frames
-binder4 <- read.csv(file = "RAWDATA/binder4-resightings.csv", header = TRUE, sep = ",")
-binder3 <- read.csv(file = "RAWDATA/binder3-resightings.csv", header = TRUE, sep = ",")
-binder2 <- read.csv(file = "RAWDATA/binder2-resightings.csv", header = TRUE, sep = ",")
-binder0 <- read.csv(file = "RAWDATA/binder0-resightings.csv", header = TRUE, sep = ",")
+resight_binder4 <- read.csv(file = "RAWDATA/binder4-resightings.csv", header = TRUE, sep = ",")
+resight_binder3 <- read.csv(file = "RAWDATA/binder3-resightings.csv", header = TRUE, sep = ",")
+resight_binder2 <- read.csv(file = "RAWDATA/binder2-resightings.csv", header = TRUE, sep = ",")
+resight_binder1 <- read.csv(file = "RAWDATA/binder1-resightings.csv", header = TRUE, sep = ",")
+resight_binder0 <- read.csv(file = "RAWDATA/binder0-resightings.csv", header = TRUE, sep = ",")
 
 
 
 
 # View the first few rows of each data frame to verify successful loading
-head(binder4)
-head(binder3)
-head(binder2)
-head(binder0)
+head(resight_binder4)
+head(resight_binder3)
+head(resight_binder2)
+head(resight_binder1)
+head(resight_binder0)
 # Check the structure of each data frame
-str(binder4)
-str(binder3)
-str(binder2)
-str(binder0)
+str(resight_binder4)
+str(resight_binder3)
+str(resight_binder2)
+str(resight_binder1)
+str(resight_binder0)
 
-binder3$date_resighted <- as.Date(binder3$date_resighted, format="%Y-%d-%m")
-binder4$date_resighted <- as.Date(binder4$date_resighted, format="%Y-%d-%m")
-binder2$date_resighted <- as.Date(binder2$date_resighted, format="%Y-%d-%m")
+resight_binder4$date_resighted <- as.Date(resight_binder4$date_resighted, format="%Y-%d-%m")
+resight_binder3$date_resighted <- as.Date(resight_binder3$date_resighted, format="%Y-%d-%m")
+resight_binder2$date_resighted <- as.Date(resight_binder2$date_resighted, format="%Y-%d-%m")
+resight_binder1$date_resighted <- as.Date(resight_binder1$date_resighted, format="%Y-%d-%m")
+resight_binder0$date_resighted <- as.Date(resight_binder0$date_resighted, format="%Y-%d-%m")
 
 #add a title to first column of binder2
-binder2 <- binder2 %>%
+resight_binder2 <- resight_binder2 %>%
   rename(band_num = X)
 
 #Combine columns x.1, x.2 and x.3 of binder2 into a new column called date and then incorporate it into date_resighted column
-binder2 <- binder2 %>%
+resight_binder2 <- resight_binder2 %>%
   mutate(date = paste(X.3, X.2, X.1, sep = "-")) %>%
   mutate(combine(date_resighted, date))
   
+
+# combine data from "band_num" column in all binders into a single data frame
 
