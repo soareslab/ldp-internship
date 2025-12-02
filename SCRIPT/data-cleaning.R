98a3ca127e73b9cd0ee8245aaffa218419903d41
Rodrigo Martin de Oliveira <hodorigo@hotmail.com>
2025-12-02 09:37:03 -0600

    fixing binder2 dates

--- SCRIPT/data-cleaning.R
+++ SCRIPT/data-cleaning.R
@@ -15,7 +15,14 @@ resight_binder2 <- read.csv(file = "RAWDATA/binder2-resightings.csv", header = T
 resight_binder1 <- read.csv(file = "RAWDATA/binder1-resightings.csv", header = TRUE, sep = ",")
 resight_binder0 <- read.csv(file = "RAWDATA/binder0-resightings.csv", header = TRUE, sep = ",")
 
-
+#load radio-tagging data from CSV file into R data frame
+radio_tagging_binder6 <- read.csv(file = "RAWDATA/radio_tagging_binder6.csv", header = TRUE, sep = ",")
+radio_tagging_binder5 <- read.csv(file = "RAWDATA/radio_tagging_binder5.csv", header = TRUE, sep = ",")
+radio_tagging_binder4 <- read.csv(file = "RAWDATA/radio_tagging_binder4.csv", header = TRUE, sep = ",")
+radio_tagging_binder3 <- read.csv(file = "RAWDATA/radio_tagging_binder3.csv", header = TRUE, sep = ",")
+radio_tagging_binder2 <- read.csv(file = "RAWDATA/radio_tagging_binder2.csv", header = TRUE, sep = ",")
+radio_tagging_binder1 <- read.csv(file = "RAWDATA/radio_tagging_binder1.csv", header = TRUE, sep = ",")
+radio_tagging_binder0 <- read.csv(file = "RAWDATA/radio_tagging_binder0.csv", header = TRUE, sep = ",")
 
 
 # View the first few rows of each data frame to verify successful loading
@@ -43,9 +50,16 @@ resight_binder2 <- resight_binder2 %>%
 
 #Combine columns x.1, x.2 and x.3 of binder2 into a new column called date and then incorporate it into date_resighted column
 resight_binder2 <- resight_binder2 %>%
-  mutate(date = paste(X.3, X.2, X.1, sep = "-")) %>%
-  mutate(combine(date_resighted, date))
+  mutate(date = paste(X.3, X.2, X.1, sep = "-")) %>% 
+  mutate(date_combined = paste(date_resighted, date))
   
+#head(resight_binder2)
+#resight_binder2$date_resighted <- as.Date(resight_binder2$date_combined, format="%Y-%d-%m")
+
+# remove NA values from date_combined column in binder2 data frame
+resight_binder2 <- resight_binder2 %>%
+  filter(!is.na(date_combined))
 
 # combine data from "band_num" column in all binders into a single data frame
+all_band_num_data <- bind_rows(resight_binder4$band_num, resight_binder3$band_num, resight_binder2$band_num, resight_binder1$band_num, resight_binder0$band_num)
 
