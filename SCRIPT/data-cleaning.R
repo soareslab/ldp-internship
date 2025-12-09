34c7636c81b84d81ff3675e1f999220507119e01
Rodrigo Martin de Oliveira <hodorigo@hotmail.com>
2025-12-09 14:03:21 -0600

    Adding the .csv from Mercy

--- SCRIPT/data-cleaning.R
+++ SCRIPT/data-cleaning.R
@@ -3,10 +3,12 @@
 
 ##install.packages("dplyr")
 ##install.packages("lubridate")
+##install.packages("ggplot2")
 
 ## Load necessary libraries
 library(dplyr)
 library(lubridate)
+library(ggplot2)
 renv::snapshot()
 
 
@@ -16,6 +18,7 @@ resight_binder3 <- read.csv(file = "RAWDATA/binder3-resightings.csv", header = T
 resight_binder2 <- read.csv(file = "RAWDATA/binder2-resightings.csv", header = TRUE, sep = ",")
 resight_binder1 <- read.csv(file = "RAWDATA/binder1-resightings.csv", header = TRUE, sep = ",")
 resight_binder0 <- read.csv(file = "RAWDATA/binder0-resightings.csv", header = TRUE, sep = ",")
+SD_pond_coords_in <- read.csv(file = "RAWDATA/SD_pond_coords_in.csv", header = TRUE, sep = ",")
 
 #load radio-tagging data from CSV file into R data frame
 radio_tagging_binder6 <- read.csv(file = "RAWDATA/radio_tagging_binder6.csv", header = TRUE, sep = ",")
@@ -35,6 +38,11 @@ resight_binder2 <- resight_binder2 %>%
 # check the first row and column names of binder2
 head(resight_binder2)
 
+# 
+
+
+
+
 
 #Combine columns x.1, x.2 and x.3 of binder2 into a new column called date and then incorporate it into date_resighted column
 resight_binder2 <- resight_binder2 %>%
@@ -49,7 +57,7 @@ sight_binder2 <- resight_binder2 %>%
     # Merge 'date' with existing 'date_resighted' row-wise
     date_resighted = paste(date_resighted, date, sep = "-"))
 
-#head(resight_binder2)
+head(resight_binder2)
 #resight_binder2$date_resighted <- as.Date(resight_binder2$date_combined, format="%Y-%d-%m")
 
 # remove NA values from date_combined column in binder2 data frame
@@ -73,12 +81,64 @@ unique_bands <- bind_rows(
   resight_binder3 %>% select(band_num),
   resight_binder2 %>% select(band_num),
   resight_binder1 %>% select(band_num),
-  resight_binder0 %>% select(band_num)
+  resight_binder0 %>% select(band_num),
+  radio_tagging_binder0 %>% select(band_num),
+  radio_tagging_binder1 %>% select(band_num),
+  radio_tagging_binder2 %>% select(band_num),
+  radio_tagging_binder3 %>% select(band_num),
+  radio_tagging_binder4 %>% select(band_num),
+  radio_tagging_binder5 %>% select(band_num),
+  radio_tagging_binder6 %>% select(band_num)
 ) %>%
   distinct()
 
+resight_binder4
+band_num\
+
+nrow(unique_band_species)
+length(unique(unique_band_species$band_num))
+
+
+# unique_band_species data frame with unique band_num and species combinations
+unique_band_species <- bind_rows(
+  resight_binder4 %>% select(band_num),
+  resight_binder3 %>% select(band_num),
+  resight_binder2 %>% select(band_num),
+  resight_binder1 %>% select(band_num),
+  resight_binder0 %>% select(band_num),
+    radio_tagging_binder0 %>% select(band_num, species),
+    radio_tagging_binder1 %>% select(band_num, species),
+    radio_tagging_binder2 %>% select(band_num, species),
+    radio_tagging_binder3 %>% select(band_num, species),
+    radio_tagging_binder4 %>% select(band_num, species),
+    radio_tagging_binder5 %>% select(band_num, species),
+    radio_tagging_binder6 %>% select(band_num, species)
+  ) %>%
+  distinct()
+
+unique_band_species$species [is.na(unique_band_species$species)] <- "Unknown"
+
+# export "unique_band_species" data frame to CSV file
+write.csv(unique_band_species, file = "CLEANDATA//band_num_species.csv", row.names = TRUE, col.names = NA)
+
+str(band_num_species)
+str(unique_bands)
+
+# compairing band_num_species to unique_bands
+du <- intersect(band_num_species$band_num, unique_bands$band_num)
+
+# Select the six first characters from "band_num" column and crate a new column called "just_band"
+unique_bands <- unique_bands %>%
+  mutate(just_band = substr(band_num, 1, 10))
+
+
+# Export "unique_bands" data frame to CSV 
+write.csv(unique_bands, file = "CLEANDATA/unique_bands.csv")
+
+
+
 # export "unique_bands" data frame to CSV file
-write.csv(unique_bands, file = "CLEANDATA//unique_bands.csv")
+#write.csv(unique_bands, file = "CLEANDATA//unique_bands.csv")
 
 
 # get unique values from "location" column in all binders into a single data frame
@@ -92,9 +152,22 @@ unique_locations <- bind_rows(
   distinct()
 
 # export "location" data frame to CSV file
-write.csv(unique_locations, file = "CLEANDATA//unique_locations.csv")
+#write.csv(unique_locations, file = "CLEANDATA//unique_locations.csv")
 
 
+# Plot the distribution of resightings over time for each binder using ggplot2
+ggplot() +
+  geom_histogram(data = resight_binder4, aes(x = date_resighted), binwidth = 30, fill = "blue", alpha = 0.5) +
+  geom_histogram(data = resight_binder3, aes(x = date_resighted), binwidth = 30, fill = "red", alpha = 0.5) +
+  geom_histogram(data = resight_binder2, aes(x = date_resighted), binwidth = 30, fill = "green", alpha = 0.5) +
+  geom_histogram(data = resight_binder1, aes(x = date_resighted), binwidth = 30, fill = "purple", alpha = 0.5) +
+  geom_histogram(data = resight_binder0, aes(x = date_resighted), binwidth = 30, fill = "orange", alpha = 0.5) +
+  labs(title = "Distribution of Resightings Over Time",
+       x = "Date Resighted",
+       y = "Count of Resightings") +
+  theme_minimal()
+renv::snapshot()
 
-
-
+# Save the plot to a file "PICTURES"
+ggsave("PICTURES/resightings_distribution.png")
+renv::snapshot()
